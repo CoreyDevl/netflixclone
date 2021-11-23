5691604807b6b2839234dc3fdab04b1da1990f2b
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-23 14:16:07 -0300

    link com google fonts
    adicionado link para fonte  e tambem algumas alterações visuais em css

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -12,7 +12,7 @@ export default ({title, items, key}) => {
   
   {items.results.length > 0 && items.results.map((item, key)=>(
       <div key={key} className="movieRow-item">
-          <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+          <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
       </div>
   ))}
   </div>
