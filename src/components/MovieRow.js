08ec013199c6ba5f2fb8487961a6ee503b96a7cd
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-23 13:46:41 -0300

    tudo ok

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,17 +1,24 @@
 import React from "react"
 import './MovieRow.css'
 
-export default ({title, items}) => {
+export default ({title, items, key}) => {
+
+  
   return (
-    <div>
-    <h2>{title}</h2>  
-    <div className="movieRow--listarea">
     
-       
-    </div>
+<div className="movieRow">
+<h2>{title}</h2>
+  <div className="movieRow-listarea">
+  
+  {items.results.length > 0 && items.results.map((item, key)=>(
+      <div key={key} className="movieRow-item">
+          <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+      </div>
+  ))}
   </div>
+</div>       
   )
-  console.log(items)
+
 }
 /* 
     {items.results.length > 0 && items.results.map((item, key)=>(
