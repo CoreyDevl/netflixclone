2d31cb581b9586b1aaec938982f5066e47437f40
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-23 16:13:35 -0300

    ajustes na area do filme em destaque
    feito as configurações css para Area do filme em destaque.
    proximo  passo  sera  adicionar as infomrações do filme em Destaque.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css'
-
+import FeaturedMovie from './components/FeaturedMovie';
 export default () => {
 
   const [movieList, setMovielist] = useState([])
-
+  const [featuredData, setFeaturedData] = useState(null)
 
  useEffect(() => {
    const loadAll = async () => {
     //pegando lista total
     let list = await Tmdb.getHomeList()
     setMovielist(list); 
+
+    //pegando o filme em destaque
+    let originals = list.filter(i=>i.slug === 'originals')
+    let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+    let chosen = originals[0].items.results[randomChosen];
+    let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')  
+    setFeaturedData(chosenInfo)
    }
 
    loadAll()
@@ -21,6 +28,9 @@ export default () => {
 
 return(
   <div className="page">
+    { featuredData &&
+    <FeaturedMovie item={featuredData}/>
+    }
 <section className="lists">
   {movieList.map((item, key)=>(
   <MovieRow key={key} title={item.title} items={item.items}/>
