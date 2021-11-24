95c11b02948b8614b6f2f7ce5789c73c19d20cd2
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-24 11:35:57 -0300

    Novas ediçoes css no featured movie

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -25,8 +25,8 @@ export default ({item})=>{
           <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1? 's' : ''}</div>
           <div className="featured--description">{item.overview}</div>
           <div className="featured--buttons">
-            <a href="#">Assistir</a>
-            <a href="#">+ Minha Lista</a>
+            <a href={`/watch/${item.id}`}>▶ Assistir</a>
+            <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
           </div>
           <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
 
